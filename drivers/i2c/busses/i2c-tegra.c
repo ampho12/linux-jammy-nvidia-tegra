28532b7fc601074f984ad8a0d3bdd6d6049c4124
Wayne Wang(SW-TEGRA) <waywang@nvidia.com>
2025-07-21 16:59:01 +0800

    i2c: tegra: Do not mark ACPI devices and PREEMPT_RT as irq safe
    That patch includes commit 14d069d92951a3e150c0a81f2ca3b93e54da913b upstream.
    
    On ACPI machines, the tegra i2c module encounters an issue due to a
    mutex being called inside a spinlock. This leads to the following bug:
    
    	BUG: sleeping function called from invalid context at kernel/locking/mutex.c:585
    	...
    
    	Call trace:
    	__might_sleep
    	__mutex_lock_common
    	mutex_lock_nested
    	acpi_subsys_runtime_resume
    	rpm_resume
    	tegra_i2c_xfer
    
    The problem arises because during __pm_runtime_resume(), the spinlock
    &dev->power.lock is acquired before rpm_resume() is called. Later,
    rpm_resume() invokes acpi_subsys_runtime_resume(), which relies on
    mutexes, triggering the error.
    
    To address this issue, devices on ACPI are now marked as not IRQ-safe,
    considering the dependency of acpi_subsys_runtime_resume() on mutexes.
    
    Also, do not mark IRQ safe in PREEMPT_RT case as RT PM path may sleep.
    And that leads to below error as RPM may not completing in time and lead
    to RCU blocking.
    
    	Call trace:
    	_switch_to
    	_schedule
    	preempt_schedule_irq
    	el1_interrupt
    	el1h_64_irq_handler
    	el1h_64_irq
    	clk_core_disable
    	clk_disable
    	clk_bulk_disable
    	tegra_i2c_runtime_suspend
    	pm_generic_runtime_suspend
    
    Bug 5354599
    Bug 5398364
    
    Change-Id: Id05ae650f27495357cec1cba72b0f75fcb8befa4
    Signed-off-by: Wayne Wang(SW-TEGRA) <waywang@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/canonical/linux-jammy/+/3413225
    Reviewed-by: Dane Liu <danel@nvidia.com>
    Reviewed-by: Sandipan Patra <spatra@nvidia.com>
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -1936,9 +1936,9 @@ static int tegra_i2c_probe(struct platform_device *pdev)
 	 * domain.
 	 *
 	 * VI I2C device shouldn't be marked as IRQ-safe because VI I2C won't
-	 * be used for atomic transfers.
+	 * be used for atomic transfers. ACPI device and RT PM case are not IRQ safe also.
 	 */
-	if (!i2c_dev->is_vi)
+	if (!i2c_dev->is_vi && !has_acpi_companion(i2c_dev->dev) && !IS_ENABLED(CONFIG_PREEMPT_RT))
 		pm_runtime_irq_safe(i2c_dev->dev);
 
 	pm_runtime_enable(i2c_dev->dev);
